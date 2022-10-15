5eb1c9780bbbe997849d5bcf29dce4cd8e204c5c
Ff3rn4nd4 <guerrareyes_fernanda@hotmail.com>
2022-10-15 17:43:32 -0500

    metodo get y put funcionando

--- WebApiZap/WebApiZap/Controllers/ZapatosController.cs
+++ WebApiZap/WebApiZap/Controllers/ZapatosController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Drawing;
 using WebApiZap.Entidades;
 
@@ -10,14 +11,35 @@ namespace WebApiZap.Controllers
     [Route("api/zapatos")]
     public class ZapatosController: ControllerBase
     {
-        [HttpGet]
-        public ActionResult<List<Zapato>> Get()
+        private readonly ApplicationDbContext dbContext;
+
+        public ZapatosController(ApplicationDbContext context)
         {
-            return new List<Zapato>()
+            this.dbContext = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Zapato>>> Get()
+        {
+            /*return new List<Zapato>()
             {
+                datos dummy (sirvieron para crear la base)
                 new Zapato() { Id=1,Talla= 22, Color="azul" },
                 new Zapato() { Id=2,Talla= 24, Color="rosa" }
-            };
+            };*/
+
+            //ayuda a obtener un listado desde la base
+            return await dbContext.Zapatos.ToListAsync();
         }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Zapato zapato)
+        {
+            dbContext.Add(zapato);
+            await dbContext.SaveChangesAsync();
+            return Ok();
+        }
+
     }
 }
